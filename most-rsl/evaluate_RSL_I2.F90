9e5601bdbcc34faf2b98b9a6fa76a0207d68cd7b
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2022-08-02 13:40:20 -0400

    Add evaluation if integral I in terms of parameters a and b

---
+++ most-rsl/evaluate_RSL_I2.F90
@@ -0,0 +1,124 @@
+program test
+  use rsl_functions_mod
+  use monin_obukhov_functions_mod
+  use monin_obukhov_kernel
+
+  implicit none
+
+  ! + namelists
+  character(32) :: stable_option = '1'
+  real    :: rich_crit      = 2.0
+  real    :: drag_min_heat  = 1.e-05
+  real    :: drag_min_moist = 1.e-05
+  real    :: drag_min_mom   = 1.e-05
+  logical :: neutral        = .false.
+  real    :: zeta_trans     = 0.5
+!   logical :: new_mo_option  = .false.
+
+  character(32) :: rsl_option = 'none'
+  real    :: rsl_mu_1 = 0.67 ! parameter of RSL correction
+  real    :: rsl_mu_m = 2.59 ! parameter of RSL momentum correction
+  real    :: rsl_mu_t = 0.95 ! parameter of RSL heat and tracer correction
+
+
+  namelist /monin_obukhov_nml/ stable_option, rich_crit, neutral, drag_min_heat, &
+                               drag_min_moist, drag_min_mom, zeta_trans, &
+                               rsl_option, rsl_mu_1, rsl_mu_m, rsl_mu_t
+
+  ! sampling parameters
+  character(8) :: var = '' ! variable to sample
+  real    :: a = 1.0, b = 1.0
+  real    :: x0 ! start of x axis
+  real    :: x1 ! end of x axis
+  integer :: nsamples ! number of intervals
+  namelist /input_nml/ a, b, &
+       var,x0,x1,nsamples
+  ! - end of namelists
+  integer :: ios
+  integer :: i, ierr_m, ierr_t
+  character(512) :: msg
+  class(most_functions_T), pointer :: most
+  class(rsl_functions_T),  pointer :: rsl
+  real  :: x,s_m,s_t,a_inv,b_inv
+
+  ! read namelists
+  open (701, file='input.nml')
+  read (701, monin_obukhov_nml, iostat=ios, iomsg=msg)
+  if (ios/=0) then
+     write(*,*)'Error reading monin_obukhov_nml ::'//trim(msg)
+     stop 1
+  endif
+  read (701, input_nml, iostat=ios, iomsg=msg)
+  if (ios/=0) then
+     write(*,*)'Error reading input_nml ::'//trim(msg)
+     stop 1
+  endif
+
+  write(*,*)'SETTINGS:'
+  write(*,monin_obukhov_nml)
+  write(*,input_nml)
+
+  ! set up stability corrections
+  select case(trim(stable_option))
+  case('1')
+     most=>make_most1_functions(rich_crit)
+  case('2')
+     most=>make_most2_functions(rich_crit, zeta_trans)
+  case('brutsaert')
+     most=>make_brutsaert_functions(rich_crit)
+  case('neutral')
+     most => make_neutral_functions(rich_crit)
+  case default
+     write (*,*)'stable_option = "'//trim(stable_option)//'" is incorrect'
+     stop 1
+  end select
+
+  ! set up roughness sublayer (RSL) corrections
+  select case(trim(rsl_option))
+  case('none')
+     rsl=>NULL()
+  case('ridder2010')
+     rsl=>make_rsl_ridder2010_functions(rsl_mu_m,rsl_mu_t)
+  case('ghannam2022')
+     rsl=>make_rsl_ghannam2022_functions(rsl_mu_1,rsl_mu_m,rsl_mu_t)
+  case default
+     write (*,*)'rsl_option = "'//trim(rsl_option)//'" is incorrect'
+     stop 1
+  end select
+  call most%set_rsl_functions(rsl)
+
+!   write(*,'(a20," = ",g14.5)') "a",a
+  write(*,'(a20," = ",g14.5)') "1/a",1/a
+!   write(*,'(a20," = ",g14.5)') "b",b
+  write(*,'(a20," = ",g14.5)') "1/b",1/b
+
+  write(*,*)'RESULTS:'
+  write(*,'(a)')'a,b,1/a,1/b,I_m,I_h,ierr_m,ierr_t'
+  do i = 0,nsamples
+     x = x0+i*(x1-x0)/nsamples
+     select case(var)
+     case ('a')
+        a = x
+     case ('a2')
+        a = x**2
+     case ('1/a')
+        a = 1/x
+     case ('b')
+        b = x
+     case ('b2')
+        if (x<0) then
+           b = -x**2
+        else
+           b =  x**2
+        endif
+     case default
+        write (*,*) 'Incorrect sampling variable'
+        stop 1
+     end select
+     call RSL_integral_I_m(most,a,b,s_m,ierr_m)
+     call RSL_integral_I_t(most,a,b,s_t,ierr_t)
+     a_inv = 0.0;  if (a/=0.0) a_inv = 1/a
+     b_inv = 0.0;  if (b/=0.0) b_inv = 1/b
+     write(*,'(99(g14.5,:,","))') a, b, a_inv, b_inv, s_m, s_t, ierr_m, ierr_t
+  enddo
+end program test
