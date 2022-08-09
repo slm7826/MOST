b9e69ac0d327a9308065507257594e1c9b24a699
Sergey Malyshev <Sergey.Malyshev@noaa.gov>
2022-08-09 12:24:29 -0400

    Update namelist and parameters

--- most-rsl/plot-PROFILE-1.bash
+++ most-rsl/plot-PROFILE-1.bash
@@ -6,24 +6,39 @@ codeDir="."
 tooldir="../tools"
 
 pushd "$codeDir"
-make
+make PROFILE.x
 popd
 
 outroot=output/profile/unstable
-t_atm=299.7
-t_sfc=294.6
-  z0m=3.8891
-   zR=63.799
- wind=1.3591
+# t_atm=299.7
+# t_sfc=294.6
+#   z0m=3.8891
+#    zR=63.799
+#  wind=1.3591
+
+# t_atm=269.30
+# t_sfc=255.36
+#   z0m=1.8156
+#    zR=62.365
+#  wind=3.0584
+
+t_atm=300.35
+t_sfc=303.12
+  z0m=2.4474
+   zR=85.361
+ wind=2.6518
 
 cat <<EOF > input.nml
  &monin_obukhov_nml
-       stable_option =  2,
-       rich_crit = 1.0,
-       zeta_trans =  0.5,
-       rsl_option = "ghannam2022"
+       stable_option = 2
+       rich_crit = 1.0
+       zeta_trans =  0.5
+       rsl_option = 'ghannam2022'
+       ! interpolation parameters
+!       a_min=1e-6, a_max=100,   a_nsteps=100
+       b_min=-100,  b_max=1000,  b_nsteps=200
 /
- &most_nml
+ &profile_nml
        z0m = $z0m
        k_over_B = 2.0
        u_atm = $wind
